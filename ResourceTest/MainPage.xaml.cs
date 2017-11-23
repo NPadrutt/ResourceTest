8e2994842b13adbf1ea185a9535700bec1bc1375
Nino Padrutt <nino.padrutt@hotmail.com>
2017-11-24 00:06:32 +0100

    init

---
+++ ResourceTest/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+using ResourceTest.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ResourceTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            TextBlock.Text = Strings.TestString;
+        }
+    }
+}
